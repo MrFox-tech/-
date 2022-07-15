b4ed779546cee9c15e24a1ffa0bb283f46cf7b00
Ivan <vanyaboguslavskii@gmail.com>
2022-07-15 22:50:50 +0300

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 $(function () {
   $('.dropdown__btn').on('click', function () {
-    $('.dropdown__content').toggleClass('dropdown__content--open')
+    $('.dropdown__content').toggleClass('dropdown__content--open');
   });
 });
 
 // Закройте выпадающее меню, если пользователь щелкает за его пределами
 window.onclick = function (event) {
   if (!event.target.matches('.dropdown__btn')) {
-    var dropdowns = document.getElementsByClassName("dropdown__content");
+    var dropdowns = document.getElementsByClassName('dropdown__content');
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
@@ -16,4 +16,4 @@ window.onclick = function (event) {
       }
     }
   }
-}
+};
